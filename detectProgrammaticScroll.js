170d8173f99588aac473e2d5607294a3103ad457
Majid Valipour <majidvp@chromium.org>
2015-04-09 12:04:28 -0400

    Merge rbyers updates

--- detectProgrammaticScroll.js
+++ detectProgrammaticScroll.js
@@ -1,10 +1,16 @@
 (function(){
   // Override function in given prototype to add a log statement 
   function logMethod(prototype, fname) {
+    if (!(fname in prototype)) {
+      console.warn("Warning: can't instrument " + prototype.constructor.name + '.' + fname);
+      return;
+    }
     var original =  prototype[fname];
     
     prototype[fname] = function() {
-      console.log(prototype.constructor.name + '::' + fname + ' invoked with ' + JSON.stringify(Array.prototype.slice.call(arguments)));
+      // Want single line output but with optional callstack - abuse 'error' for this.
+      // console.trace is also good but expands the callstack by default.'
+      console.error(prototype.constructor.name + '::' + fname + ' invoked with ' + JSON.stringify(Array.prototype.slice.call(arguments)));
       // debugger;
       original.apply(this, arguments);
     };
@@ -13,11 +19,16 @@
   // Override property setter to add a log statement
   function logSetter(prototype, property) {
     var original = Object.getOwnPropertyDescriptor(prototype, property);
+    if (!original) {
+      console.warn("Warning: can't instrument " + prototype.constructor.name + '.' + property + 
+        '. Chrome 43+ required (http://crbug.com/43394)');
+      return;
+    }
     
     Object.defineProperty(prototype, property, {
       set: function(v) {
         var changed = v !== original.get.call(this);
-        console.log(prototype.constructor.name + '::' + property + ' set to:' + v + (changed?' (changed)':' (not changed)')); // add changed indicator
+        console.error(prototype.constructor.name + '::' + property + ' set to:' + v + (changed?' (changed)':' (not changed)'));
         // debugger;
         original.set.call(this, v);
       },
@@ -25,17 +36,16 @@
     });
   }
   
-  var windowScrollMethods = ['scrollBy', 'scrollTo', 'scroll'];
-  for (var i = 0; i < windowScrollMethods.length; i++)
-    logMethod(Window.prototype, windowScrollMethods[i]);
+  for (var m of ['scrollBy', 'scrollTo', 'scroll'])
+    logMethod(Window.prototype, m);
+ 
+  // Doesn't yet work in Chrome - http://crbug.com/475556
+  for (var p of ['scrollX', 'scrollY'])
+    logSetter(Window.prototype, p);
   
-  var elementScrollMethods = ['scrollIntoView', 'scrollIntoViewIfNeeded', 'focus'];
-  for (var i = 0; i < elementScrollMethods.length; i++)
-    logMethod(Element.prototype, elementScrollMethods[i]);
+  for (var m of ['scrollIntoView', 'scrollIntoViewIfNeeded', 'focus'])
+    logMethod(Element.prototype, m);
   
-  // Note that this requires DOM attributes to be defined on prototype chain
-  // which is only true for Chrome version >= 44
-  var scrollProperties = ['scrollTop', 'scrollLeft'];
-  for (var i = 0; i < scrollProperties.length; i++)
-    logSetter(Element.prototype, scrollProperties[i]);
+  for (var p of ['scrollTop', 'scrollLeft'])
+    logSetter(Element.prototype, p);
 })();
